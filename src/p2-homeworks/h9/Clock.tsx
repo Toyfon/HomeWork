402ae7c5e163c56c0254e0668b33fb8315848a44
Toyfon <90236783+Toyfon@users.noreply.github.com>
2021-12-09 12:36:25 +0300

    add HW9

--- src/p2-homeworks/h9/Clock.tsx
+++ src/p2-homeworks/h9/Clock.tsx
@@ -1,5 +1,6 @@
 import React, {useState} from 'react'
 import SuperButton from '../h4/common/c2-SuperButton/SuperButton'
+import s from './HW9.module.css'
 
 function Clock() {
     const [timerId, setTimerId] = useState<number>(0)
@@ -7,44 +8,44 @@ function Clock() {
     const [show, setShow] = useState<boolean>(false)
 
     const stop = () => {
-        // stop
+        clearInterval(timerId)
     }
     const start = () => {
         stop()
         const id: number = window.setInterval(() => {
-            // setDate
+            setDate(new Date())
         }, 1000)
         setTimerId(id)
     }
 
     const onMouseEnter = () => {
-        // show
+        setShow(true)
     }
     const onMouseLeave = () => {
-        // close
+        setShow(false)
     }
 
-    const stringTime = 'Time' // fix with date
-    const stringDate = 'Date' // fix with date
+    const stringTime = date?.toLocaleTimeString() || <br/>
+    const stringDate = date?.toLocaleDateString() || <br/>
 
     return (
-        <div>
-            <div
+        <div className={s.container}>
+            <div className={s.time}
                 onMouseEnter={onMouseEnter}
                 onMouseLeave={onMouseLeave}
             >
                 {stringTime}
             </div>
 
-            {show && (
-                <div>
+            {show ? (
+                <div className={s.date}>
                     {stringDate}
                 </div>
-            )}
-
-            <SuperButton onClick={start}>start</SuperButton>
-            <SuperButton onClick={stop}>stop</SuperButton>
-
+            ) : <br/>}
+            <div className={s.buttons}>
+                <SuperButton className={s.btn} onClick={start}>start</SuperButton>
+                <SuperButton className={s.btn} onClick={stop}>stop</SuperButton>
+            </div>
         </div>
     )
 }
