6cd6012d0ae49b29577d44a04591e616f936e056
Toyfon <90236783+Toyfon@users.noreply.github.com>
2021-12-18 18:31:43 +0300

    add HW11

--- src/p2-homeworks/h11/HW11.tsx
+++ src/p2-homeworks/h11/HW11.tsx
@@ -1,10 +1,13 @@
 import React, {useState} from 'react'
 import SuperRange from './common/c7-SuperRange/SuperRange'
 import SuperDoubleRange from './common/c8-SuperDoubleRange/SuperDoubleRange'
+import s from './HW11.module.css'
 
 function HW11() {
-    const [value1, setValue1] = useState(0)
+    const [value1, setValue1] = useState(50)
     const [value2, setValue2] = useState(100)
+    const [value3, setValue3] = React.useState<number[]>([value1, value2])
+
 
     return (
         <div>
@@ -12,16 +15,25 @@ function HW11() {
             homeworks 11
 
             {/*should work (должно работать)*/}
-            <div>
-                <span>{value1}</span>
-                <SuperRange
+            <div  className={s.rangeBlock}>
+                <span>{value1 < value2 ? value1 : value2 -1}</span>
+                <SuperRange value1={value1}
+                            value2={value2}
+                            setValue3={setValue3}
+                            onChangeRange={setValue1}
                     // сделать так чтоб value1 изменялось
                 />
             </div>
 
-            <div>
-                <span>{value1}</span>
+            <div  className={s.doubleRangeBlock}>
+                <span>{value1 < value2 ? value1 : value2 -1}</span>
                 <SuperDoubleRange
+                    value1={value1}
+                    value2={value2}
+                    value3={value3}
+                    setValue1={setValue1}
+                    setValue2={setValue2}
+                    setValue3={setValue3}
                     // сделать так чтоб value1 и value2 изменялось
                 />
                 <span>{value2}</span>
--- src/p2-homeworks/h11/common/c8-SuperDoubleRange/SuperDoubleRange.tsx
+++ src/p2-homeworks/h11/common/c8-SuperDoubleRange/SuperDoubleRange.tsx
@@ -1,24 +1,64 @@
 import React from 'react'
+import {Box, Slider} from "@mui/material";
+
 
 type SuperDoubleRangePropsType = {
-    onChangeRange?: (value: [number, number]) => void
-    value?: [number, number]
-    // min, max, step, disable, ...
+    setValue1: (value1:number) =>void
+    setValue2: (value2:number) =>void
+    setValue3: (value3:number[])=> void
+    value1:number
+    value2:number
+    value3:number[]
 }
+
+const minDistance = 2;
 
 const SuperDoubleRange: React.FC<SuperDoubleRangePropsType> = (
     {
-        onChangeRange, value,
-        // min, max, step, disable, ...
+        setValue1,
+        setValue2,
+        setValue3,
+        value1,
+        value2,
+        value3,
+       ...restProps
     }
 ) => {
-    // сделать самому, можно подключать библиотеки
+
+
+    const handleChange1 = (
+        event: Event,
+        newValue: number | number[],
+        activeThumb: number,
+    ) => {
+        if (!Array.isArray(newValue)) {
+            return;
+        }
+
+        if (activeThumb === 0) {
+            setValue3([Math.min(newValue[0], value2 - minDistance), value2]);
+            setValue1(Math.min(newValue[0], value2 - minDistance))
+        } else {
+            setValue3([value3[0], Math.max(newValue[1], value1 + minDistance)]);
+            setValue2(value3[1])
+        }
+    };
+
 
     return (
         <>
-            DoubleRange
+            <Box sx={{ width: 300, marginRight: "25px" }}>
+                <Slider
+                    size={'medium'}
+                    value={value3}
+                    onChange={handleChange1}
+                    style={{color:"teal"}}
+                />
+            </Box>
+
         </>
     )
 }
 
 export default SuperDoubleRange
+
